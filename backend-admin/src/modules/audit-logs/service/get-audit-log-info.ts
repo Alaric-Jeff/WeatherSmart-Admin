d0d35b6c97e3c410435b17427df43b320f752e2b
Jeffrey M. Aspiras <jefsohandsome1@gmail.com>
2026-01-18 17:37:38 +0800

    audit logs completed

---
+++ backend-admin/src/modules/audit-logs/service/get-audit-log-info.ts
@@ -0,0 +1,28 @@
+import type { FastifyInstance } from "fastify";
+import { ServiceError } from "../../../error/service-error.js";                         
+export async function getAuditInfo(
+    fastify: FastifyInstance, 
+    body: {id: string}
+){
+    try{
+        const audits = await fastify.db.collection('audit_logs').doc(body.id).get();
+
+        if(!audits.exists){
+            throw new ServiceError(404, "Audit not found");
+        }
+
+        const auditData = audits.data() ?? {}
+
+        return {
+            id: auditData.id,
+            performedBy: auditData.performedBy,
+            action: auditData.action,
+            target: auditData.target,
+            createdAt: auditData.createdAt
+        }
+
+    }catch(err: unknown){
+        fastify.log.error(`error occured in get audit log, error: ${err}`);
+        throw new ServiceError(500, "Internal Server Error");
+    }
+}
