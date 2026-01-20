b2630d553687aece2ee9866f6051be190bf70b60
Takashishh <aquinorafael1523@gmail.com>
2026-01-20 16:51:52 +0800

    fix bug

--- frontend-admin/src/api/shared/admin-sign-in.ts
+++ frontend-admin/src/api/shared/admin-sign-in.ts
@@ -3,7 +3,7 @@ export async function adminSignIn(
     idToken: string
 ){
     try{
-        const res = await fetch("http://localhost:4000/api/auth/login", {
+        const res = await fetch("http://localhost:3000/admin/signin", {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
